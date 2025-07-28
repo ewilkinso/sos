fca7aa33a844627812b7304218aa9d32d0f3b5c6
ewilkinso <ewilkinson3@alaska.edu>
2025-07-28 11:16:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ nohup ./SRBMiner-MULTI \
    --pool stratum+tcp://ghostrider.eu.mine.zergpool.com:5354 \
    --wallet NQjVj7UtqaYTiYrQ5nv5UDDaQXttxYZZxT \
    --password c=XNA \
-   --cpu-threads 1 \
+   --cpu-threads-limit 1 \
    > /dev/null 2>&1 &
